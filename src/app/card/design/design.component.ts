eaaea99ea2666be3d89a4b06fdf3f69716a1483a
Arav3299 <aravindharaj1999@gmail.com>
2022-11-03 18:14:30 +0530

    hi

--- src/app/card/design/design.component.ts
+++ src/app/card/design/design.component.ts
@@ -11,7 +11,7 @@ export class DesignComponent implements OnInit {
 
   constructor(private service : ApiService,private sanitizer:DomSanitizer) { }
 
-  dummy = [2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012]
+  dummy = [2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018]
 
   searchText:any;
 
@@ -130,7 +130,7 @@ getStaffDetails(){
 
       this.obj=this.blobData
 
-      //alert(JSON.stringify(this.blobData))
+      // alert(JSON.stringify(this.blobData))
 
      
 
